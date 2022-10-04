023937ebb0e5043946244101f9480636d84d0514
unknown <sakethpippari@gmail.com>
2022-10-04 17:39:34 +0530

    script 1

---
+++ 1stproject.js
@@ -0,0 +1,53 @@
+//let price1=50;
+//let price2=100;
+//let total=price1+price2;
+//console.log(total);
+let a=20;
+let b=10;
+let c=a+b;
+let c1=a-b;
+let c2=a*b;
+let c3=a/b;
+let c4=a%b;
+console.log(c,c1,c2,c3,c4)
+//document.write('welcome js')
+//alert('hey js')
+let n1=20;
+let n2=30;
+let n3=n1<n2;
+let n4=n1>n2;
+let n5=n1<=n2;
+let n6=n1>=n2;
+let n7=n1==n2;
+console.log(n3,n4,n5,n6,n7);
+var s="radio"
+var t="button"
+console.log(t)
+let s1=20;
+let s2=30;
+console.log(s1>s2 && s1<s2);
+console.log(s1>s2 || s1<s2);
+console.log(s1!=s2);
+let t1=5;
+let t2=8;
+let t3=t1--;
+let t4=t2++;
+console.log(t1);
+console.log(t2);
+let h1=5;
+let h2=1;
+let h3=h1&h2;
+let h4=h1|h2;
+let h5=h1<<h2;
+let h6=h1>>h2;
+let h7=h1^h2;
+let h8=~ h1;
+console.log(h3,h4,h5,h6,h7,h8);
+let x1=10;
+let x2=20;
+let x3=((x1>x2)?true:false)
+let x4=((x1<x2)?true:false)
+console.log(x3,x4);
+let z1=typeof "john"
+let z2=typeof 5.52
+console.log(z1,z2)
